13b8fdb02a8c21be22e167b9ab275bbd6b28a7d3
shojs777 <shokiebi2561@gmail.com>
2024-03-15 04:05:30 +0900

    スタイル修正、記事詳細ページ作成

--- src/lib/spearly.ts
+++ src/lib/spearly.ts
@@ -1,5 +1,15 @@
 import { SpearlyApiClient } from "@spearly/sdk-js";
 
 const apiClient = new SpearlyApiClient(import.meta.env.SPEARlY_API_KEY);
+const fetchPostsData = async () => {
+  return await apiClient.getList("sample-post");
+};
+const fetchLimitPostsData = async (perPage: number, pageNum: number) => {
+  const data = await apiClient.getList("sample-post", {
+    limit: perPage,
+    offset: 1 + (pageNum - 1) * perPage,
+  });
+  return data;
+};
 
-export { apiClient };
+export { fetchPostsData, fetchLimitPostsData };
