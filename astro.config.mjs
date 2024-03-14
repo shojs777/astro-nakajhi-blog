d38b7e50799ed9aa5af35c9d9301e8148d5e2dbe
shojs777 <shokiebi2561@gmail.com>
2024-03-15 04:04:17 +0900

    SSRからSSGに変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import cloudflare from "@astrojs/cloudflare";
+// import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: cloudflare(),
+  output: "static",
+  // adapter: cloudflare(),SSRする際に必要
   integrations: [tailwind()],
 });
