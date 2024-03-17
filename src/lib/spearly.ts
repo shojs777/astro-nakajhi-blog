ad93a49a1d0e82df48b494a74c9605f13051533b
shojs777 <shokiebi2561@gmail.com>
2024-03-17 23:57:17 +0900

    ページネーションの実装とスタイル修正

--- src/lib/spearly.ts
+++ src/lib/spearly.ts
@@ -1,13 +1,15 @@
 import { SpearlyApiClient } from "@spearly/sdk-js";
 
 const apiClient = new SpearlyApiClient(import.meta.env.SPEARlY_API_KEY);
+
 const fetchPostsData = async () => {
   return await apiClient.getList("sample-post");
 };
+
 const fetchLimitPostsData = async (perPage: number, pageNum: number) => {
   const data = await apiClient.getList("sample-post", {
     limit: perPage,
-    offset: 1 + (pageNum - 1) * perPage,
+    offset: pageNum <= 1 ? 0 : (pageNum - 1) * perPage + 1,
   });
   return data;
 };
