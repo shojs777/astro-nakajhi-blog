4df8c5843a167d7c1a6ae10a750aa033068f7a75
shojs777 <shokiebi2561@gmail.com>
2024-04-16 13:29:10 +0900

    記事詳細ページ修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,4 +8,8 @@ export default defineConfig({
   output: "static",
   // adapter: cloudflare(),SSRする際に必要
   integrations: [tailwind()],
+  image: {
+    // 例: リモート画像の最適化を単一のドメインに対して許可する
+    domains: ["static.spearly.com"],
+  },
 });
